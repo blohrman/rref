8ab33825fa574d20a8704d7b0d7ecbaaa50a54c3
Ben Jammin <blohrman@huskers.unl.edu>
2020-08-17 23:47:14 -0500

    successful on first test case... progress

--- src/RowReduction.java
+++ src/RowReduction.java
@@ -1,76 +1,85 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RowReduction {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double[][] test = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        test = rref(test, 0, 0);
+
+        System.out.println(Arrays.deepToString(test));
     }
 
-    // just realized that none of this works because of integer division
-    // so gonna just have to rewrite all if the int[][] shit as doubles
-    public static int[][] rref(int[][] toReduce, int currRow, int currCol) {
-        //int currRow = 0; <- implied at the start, leaving so i remember or if
-        //recursion doesn't work
-        //int currCol = 0;
+    public static double[][] rref(double[][] toReduce, int currRow, int currCol) {
 
-        // checks if swapping a row will produce a one
-        if (toReduce[currRow][currCol] == 1) {
-            // be happy
+        if (currRow == toReduce.length || currCol == toReduce[0].length) {
+
+            // reduce up
+            reduceUp(toReduce, --currRow, --currCol);
+
+            return toReduce;
         } else {
-            int nextRow = currRow + 1;
-            List<Integer> rowNums = new ArrayList<>();
-            for (int i = nextRow; i < toReduce[currRow].length; i++) {
-                if (toReduce[i][currCol] == 1) {
-                    swapRow(toReduce, currRow, nextRow);
-                    break;
-                } else {
-                    rowNums.add(toReduce[i][currCol]);
-                }
-            }
-
-            // add functionality to check if adding two rows will lead to a one
-
-            // checks if scaling a row will produce a one forwards and backwards
             if (toReduce[currRow][currCol] == 1) {
                 // be happy
-            } else {
-                for (int i = 0; i < rowNums.size(); i++) {
-                    if (toReduce[currRow][currCol] % rowNums.get(i) == 0) {
-                        scaleRow(toReduce, currRow, (1 / rowNums.get(i)));
-                        break;
-                    } else if (rowNums.get(i) % toReduce[currRow][currCol] == 0){
-                        // i - 1 accounts for the fact that rowNums excludes
-                        // first row in iteration
-                        swapRow(toReduce, currRow, i - 1);
-                        scaleRow(toReduce, currRow, (1 / rowNums.get(i)));
-                        break;
+            } else if (toReduce[currRow][currCol] == 0){
+                int zeroCount = 0;
+
+                for (int i = currRow; i < toReduce.length; i++) {
+                    if (toReduce[i][currCol] != 0) {
+                        swapRow(toReduce, i, currRow);
+                    } else {
+                        zeroCount++;
                     }
                 }
-            }
 
-            // worst case scale and combine rows to produce a one. this will
-            // be the trickiest case to do
-            if (toReduce[currRow][currCol] == 1) {
-                // be happy
+                if (zeroCount == toReduce.length - currRow) {
+                    return rref(toReduce, ++currRow, ++currCol);
+                }
             } else {
-
+                scaleRow(toReduce, currRow, (1 / toReduce[currRow][currCol]));
             }
-        }
 
-        return null;
+            System.out.println(Arrays.deepToString(toReduce));
+
+            // reduce down
+            for (int i = 1; i < toReduce.length - currRow; i++) {
+                scaleAndAddRows(toReduce, currRow, currRow + i, -(toReduce[currRow + i][currCol]));
+                System.out.println(Arrays.deepToString(toReduce));
+            }
+
+            System.out.println(Arrays.deepToString(toReduce));
+
+            return rref(toReduce, ++currRow, ++currCol);
+        }
     }
 
-    private static int[][] swapRow(int[][] toSwap, int index1, int index2) {
+    private static double[][] reduceUp(double[][] toReduce, int currRow, int currCol) {
+        if (currRow == 0 || currCol == 0) {
+            return toReduce;
+        } else {
+            for (int i = 1; i < toReduce.length - currRow; i++) {
+                scaleAndAddRows(toReduce, currRow, currRow - i, -(toReduce[currRow - i][currCol]));
+                System.out.println(Arrays.deepToString(toReduce));
+            }
 
-        int[] placeholder = toSwap[index1];
+            return reduceUp(toReduce, --currRow, --currCol);
+        }
+    }
+
+    private static double[][] swapRow(double[][] toSwap, int index1, int index2) {
+
+        double[] placeholder = toSwap[index1];
         toSwap[index1] = toSwap[index2];
         toSwap[index2] = placeholder;
 
         return toSwap;
     }
 
-    private static int[][] scaleRow(int[][] toScale, int index, int scalar) {
+    private static double[][] scaleRow(double[][] toScale, int index, double scalar) {
 
         for (int i = 0; i < toScale[index].length; i++) {
             toScale[index][i] *= scalar;
@@ -79,9 +88,29 @@ public class RowReduction {
         return toScale;
     }
 
-    private static int[][] addRows(int[][] toCombine, int index1, int index2) {
+    private static double[][] addRows(double[][] toCombine, int index1, int index2) {
         for (int i = 0; i < toCombine[index1].length; i++) {
             toCombine[index1][i] += toCombine[index2][i];
+        }
+
+        return toCombine;
+    }
+
+    private static double[][] scaleAndAddRows(double[][] toCombine, int scaleRowIndex, int addRowIndex, double scalar) {
+        double[] scaledRow = toCombine[scaleRowIndex];
+
+        double[] scaledRow2 = new double[toCombine[scaleRowIndex].length];
+
+        for (int i = 0; i < scaledRow2.length; i++) {
+            scaledRow2[i] = toCombine[scaleRowIndex][i];
+        }
+
+        for (int i = 0; i < scaledRow2.length; i++) {
+            scaledRow2[i] *= scalar;
+        }
+
+        for (int i = 0; i < toCombine[addRowIndex].length; i++) {
+            toCombine[addRowIndex][i] += scaledRow2[i];
         }
 
         return toCombine;
